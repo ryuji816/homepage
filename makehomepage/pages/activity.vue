2852c090130373fb0ef05f3a3a051a0b1d445e95
hirosuke29 <h2so4@mineo.jp>
2020-10-27 00:08:44 +0900

    made page look good

--- makehomepage/pages/activity.vue
+++ makehomepage/pages/activity.vue
@@ -23,10 +23,10 @@
             xs="12"
           >
             <div class="text-h2 text-center">
-              <span class="artitle">もくもく会</span>
+              <span class="activity_title">もくもく会</span>
             </div>
             <hr class="primary" color="primary">
-            <p class="text-justify content text">
+            <p class="text-justify content activity_text">
               もくもく会とは主にプログラミングなどの作業・勉強などをする目的で複数人が集まり、"黙々と"作業をするイベントです。 集まって作業することで集中して作業ができ、困ったら相談するなどして効率的な作業や勉強が行えます。そして最後にはその日行った作業や勉強内容を各人が共有することで知見を広げたり、発表や質疑応答を通して自分自身の理解を深めることができます。
             </p>
           </v-col>
@@ -47,10 +47,10 @@
             xs="12"
           >
             <div class="text-h2 text-center">
-              <span class="artitle">競技プログラミング振り返り会</span>
+              <span class="activity_title">競技プログラミング</span>
             </div>
             <hr class="primary" color="primary">
-            <p class="text-justify content text">
+            <p class="text-justify content activity_text">
               毎週末に開催されるAtCoderなどの競技プログラミングのコンテストの振り返りを行います。Pabloには競技プログラミングに参加しているメンバーが多数在籍しており、コンテスト結果の共有・解けなかった問題の復習などを行っています。
             </p>
           </v-col>
@@ -89,10 +89,10 @@
             xs="12"
           >
             <div class="text-h2 text-center">
-              <span class="artitle">自主ゼミ</span>
+              <span class="activity_title">自主ゼミ</span>
             </div>
             <hr class="primary" color="primary">
-            <p class="text-justify content text">
+            <p class="text-justify content activity_text">
               Pabloでは有志自主ゼミを開催しています。プログラミングだけではなく、数学など多岐にわたる分野の勉強を行っています。1人では勉強しにくい文献の少ない分野についても、仲間と楽しく勉強することができます。
             </p>
           </v-col>
@@ -113,10 +113,10 @@
             xs="12"
           >
             <div class="text-h2 text-center">
-              <span class="artitle">受託開発</span>
+              <span class="activity_title">受託開発</span>
             </div>
             <hr class="primary" color="primary">
-            <p class="text-justify content text">
+            <p class="text-justify content activity_text">
               お客様の思い描くサービスを具体的な形にする開発を行っています。特に大学生の方などからご相談をいただく事が多いです。熱い志を持った方のプロジェクトを全力でサポートします。
             </p>
           </v-col>
@@ -155,10 +155,10 @@
             xs="12"
           >
             <div class="text-h2 text-center">
-              <span class="artitle">自社開発</span>
+              <span class="activity_title">自社開発</span>
             </div>
             <hr class="primary" color="primary">
-            <p class="text-justify content text">
+            <p class="text-justify content activity_text">
               メンバーが考えた企画を具体的なサービスにする活動を行っています。企画からシステム開発、営業まで全てサークル内で行います。
             </p>
           </v-col>
@@ -174,27 +174,42 @@ export default {
   layout: 'lactivity'
 }
 </script>
+
 <style scoped>
+@media screen and (max-width: 420px) {
 .odd {
   background-color: white;
   background-size: cover;
   background-position: center center;
   width: 100%;
-  height: 830px;
+  height: auto;
 }
 .even {
   background-color: whitesmoke;
   background-size: cover;
   background-position: center center;
   width: 100%;
-  height: 830px;
+  height: auto;
 }
-.artitle {
+}
+.odd {
+  background-color: white;
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+}
+.even {
+  background-color: whitesmoke;
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+}
+.activity_title {
   font-family: 'Courier New', Courier, monospace;
   font-size: 50px;
   color: black;
 }
-.text {
+.activity_text {
   line-height: 40px;
   font-family: 'Times New Roman', Times, serif;
   font-size: 20px;
